a2a57ac0c4309e28c0f497bcba8df3fa01d7c135
Pratik Gurung <pratikgurung022@gmail.com>
2026-02-09 21:02:29 -0800

    Added an actual new ai agent running sequentially

--- llm/client.py
+++ llm/client.py
@@ -1,5 +1,7 @@
 import os
 from google import genai
+from openai import OpenAI
+
 
 from dotenv import load_dotenv 
 
@@ -7,19 +9,17 @@ from dotenv import load_dotenv
 def sayAI(user):
     print("Hello", user)
 
-def config():
-    load_dotenv()
+
+load_dotenv()
 
 #Actual code:
-class AIBot:
-    
-    config()
 
+class AIBot:
     def __init__(self):
         self.client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
         self.chats = self.client.chats.create(model="gemini-2.5-flash")
 
-    def callModel(self, userPromptText):
+    def call(self, userPromptText):
 
         responseAi = self.chats.send_message_stream(userPromptText)
 
@@ -31,7 +31,28 @@ class AIBot:
 
         return fullUserText
 
+class SecondAIBot:
+    def __init__(self):
+        key = os.getenv("NEBIUS_API_KEY")
 
-SEGAI = AIBot()
+        if not key:
+            raise ValueError("Missing or invalid NEBIUS_API_KEY")
+
+        self.client = OpenAI(api_key = key,
+                     base_url="https://api.tokenfactory.nebius.com/v1/"        
+                    )
+        self.model = "openai/gpt-oss-120b"
+
+    
+    def call(self, userPromptText):
+
+        aiResp = self.client.chat.completions.create(
+            model = self.model,
+            messages=[{"role":"user","content": userPromptText}]
+        )
+
+        return aiResp.choices[0].message.content
+
+
 
 
